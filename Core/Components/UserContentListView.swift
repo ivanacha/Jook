ca3d49a559a459f9df101c80b828ee12cc3e3a95
Ivan C. Acha <ivanacha4@gmail.com>
2024-04-30 00:09:38 -0400

    Create UserContentListView.swift

---
+++ Core/Components/UserContentListView.swift
@@ -0,0 +1,61 @@
+//
+//  UserContentListView.swift
+//  Jook
+//
+//  Created by iVan on 4/29/24.
+//
+
+import SwiftUI
+
+struct UserContentListView: View {
+    @State private var selectedFilter: ProfileThreadFilter = .posts // Keeps track of the selected filter
+    @Namespace var animation
+    
+    private var filterBarWidth : CGFloat {
+        let count = CGFloat(ProfileThreadFilter.allCases.count)
+        return UIScreen.main.bounds.width / count - 15 // UIScreen.main to be deprecated
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(ProfileThreadFilter.allCases) {filter in
+                    VStack {
+                        Text(filter.title)
+                            .font(.subheadline)
+                            .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                        
+                        if selectedFilter == filter {
+                            Rectangle()
+                                .foregroundStyle(.black)
+                                .frame(width: filterBarWidth, height: 1)
+                                .matchedGeometryEffect(id: "item", in: animation)
+                        } else {
+                            Rectangle()
+                                .foregroundStyle(.clear)
+                                .frame(width: filterBarWidth, height: 1)
+                        }
+                    }
+                    .onTapGesture {
+                        withAnimation(.spring) {
+                            selectedFilter = filter
+                        }
+                    }
+                }
+            }
+            
+            LazyVStack {
+                ForEach(0...10, id:\.self) { post in
+                    PostCell()
+                }
+            }
+        }
+        .padding(.vertical, 8)
+    }
+}
+
+struct UserContentListView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserContentListView()
+    }
+}
